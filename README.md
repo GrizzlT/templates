5ed30026666282ecddaaf5b2175c14a94c8e35d6
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-09-09 17:20:04 +0200

    feat: init rust project template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Development templates
+
+I write a lot of Rust. I like my feet DRY.
--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         description = "Empty flake with boilerplate inputs/outputs.";
         path = ./empty;
       };
+      rustAsyncCli = {
+        description = "Rust project with config file support and async main loop";
+        path = ./rust_config_async;
+      };
     };
   };
 }
